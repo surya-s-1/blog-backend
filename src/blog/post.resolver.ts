e1a9b78199d98411e72d196e0482c0b33ff097ac
Surya S <suryah2806@gmail.com>
2025-04-20 13:55:57 +0530

    reduce db reads for post retrieval by storing user data with posts and also added public visibility field

--- src/blog/post.resolver.ts
+++ src/blog/post.resolver.ts
@@ -19,19 +19,35 @@ export class PostResolver {
         const result: Post[] = []
         
         posts.forEach(async (p) => {
-            const user: UserDocument | null = await this.userService.getUserDetails(p.uid)
+            /* If all the minimun details exist, return the post */
+            if (p.username && p.first_name && p.last_name && p.dp) {
+                result.push({
+                    pid: p.pid,
+                    content: p.content,
+                    createdAt: p.created_at,
+                    uid: p.uid,
+                    username: p.username,
+                    firstName: p.first_name,
+                    middleName: p.middle_name,
+                    lastName: p.last_name,
+                    dp: p.dp
+                })
+            /* Else, query the db for the user data and then return the post */
+            } else {
+                const user: UserDocument | null = await this.userService.getUserDetails(p.uid)
 
-            result.push({
-                pid: p.pid,
-                content: p.content,
-                createdAt: p.created_at,
-                uid: user?.uid,
-                username: user?.u_name,
-                firstName: user?.f_name,
-                middleName: user?.m_name,
-                lastName: user?.l_name,
-                dp: user?.dp
-            })
+                result.push({
+                    pid: p.pid,
+                    content: p.content,
+                    createdAt: p.created_at,
+                    uid: user?.uid,
+                    username: user?.u_name,
+                    firstName: user?.f_name,
+                    middleName: user?.m_name,
+                    lastName: user?.l_name,
+                    dp: user?.dp
+                })
+            }
         })
 
         return result
--- src/blog/schema/post.schema.ts
+++ src/blog/schema/post.schema.ts
@@ -12,8 +12,29 @@ export class Post extends Document {
     @Prop({ required: true, type: Date })
     created_at: Date
 
+    @Prop({ required: true, type: Boolean, default: true})
+    public: Boolean
+
+    @Prop({ default: [] })
+    tags: string[]
+
     @Prop({ required: true })
     uid: string
+
+    @Prop()
+    username: string
+
+    @Prop()
+    first_name: string
+
+    @Prop()
+    middle_name: string
+
+    @Prop()
+    last_name: string
+
+    @Prop()
+    dp: string
 }
 
 export const PostSchema = SchemaFactory.createForClass(Post)
